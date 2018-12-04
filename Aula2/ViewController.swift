bdf33e27ea2a9bbdfae3ccf22f98d0e13bbd5b00
Sarah Ferreira Mendes da Cunha <a21260876@deis-l15-02.local>
2018-12-04 15:02:19 +0000

    Initial Commit

---
+++ Aula2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aula2
+//
+//  Created by Sarah Ferreira Mendes da Cunha on 04/12/2018.
+//  Copyright © 2018 Sarah Ferreira Mendes da Cunha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
